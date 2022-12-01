4ec8ec05017d469e57efd31cb11e165b653d9ba5
Андрей Шмыголь <a.v.shmygol@asu.niitp.local>
2022-12-01 16:26:51 +0300

    Initial Page

---
+++ js/basic.js
@@ -0,0 +1,8 @@
+$(function() {
+  // Run Parallax
+  var scene = document.getElementById('scene');
+  var parallaxInstance = new Parallax(scene);
+
+  // Run jQuery UI Accordion
+  $("#accordion").accordion();
+});
