5027c4a4e27ba405b777653950b4c74474c2dc98
avshmygol <avshmygol@yandex.ru>
2022-12-05 16:42:18 +0300

    Update Parallax

--- js/basic.js
+++ js/basic.js
@@ -1,7 +1,7 @@
 $(function() {
   // Run Parallax
-  var scene = document.getElementById('scene');
-  var parallaxInstance = new Parallax(scene);
+  // var scene = document.getElementById('scene');
+  // var parallaxInstance = new Parallax(scene);
 
   // Run jQuery UI Accordion
   $("#accordion").accordion();
