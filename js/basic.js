897304d8d60e56cdfa89f39cf01bf5ee808ae3ed
avshmygol <avshmygol@yandex.ru>
2022-12-07 11:48:01 +0300

    Update Page

--- js/basic.js
+++ js/basic.js
@@ -1,8 +1,4 @@
 $(function() {
-  // Run Parallax
-  // var scene = document.getElementById('scene');
-  // var parallaxInstance = new Parallax(scene);
-
   // Run jQuery UI Accordion
   $("#accordion").accordion();
 });
